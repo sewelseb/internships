adbd04698a9b8d8b8f8590a26479808419198650
sewelseb <sewelseb@hotmail.com>
2020-02-15 17:13:31 +0100

    add - list of users

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\InternshipApplication;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,6 +11,12 @@ class AdminController extends AbstractController
 {
     public function index() {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin/admin.html.twig');
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        $applications = $this->getDoctrine()->getRepository(InternshipApplication::class)->findAll();
+
+        return $this->render('admin/admin.html.twig', [
+            'users' => $users,
+            'applications' => $applications
+        ]);
     }
 }
